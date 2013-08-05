7d9c7133de36d2fd7587d7be361cd0ff964d4e94
Jason Bittel <jason.bittel@gmail.com>
2013-08-05 08:14:32 -0700

    Add custom URL alias paths to URLconf

--- deflect/urls.py
+++ deflect/urls.py
@@ -1,9 +1,18 @@
+from django.conf import settings
 from django.conf.urls import patterns
 from django.conf.urls import url
 
+from .views import alias
 from .views import redirect
 
 
 urlpatterns = patterns('',
     url(r'^(?P<key>[a-zA-Z0-9]+)$', redirect, name='deflect-redirect'),
 )
+
+alias_prefix = getattr(settings, 'DEFLECT_ALIAS_PREFIX', '')
+if alias_prefix:
+    urlpatterns += patterns('',
+        url(r'^%s(?P<key>[a-zA-Z0-9]+)$' % alias_prefix, alias,
+            name='deflect-alias'),
+    )
