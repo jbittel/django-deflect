e1677c3abf43e43a21663f1416059f83a9209efa
Jason Bittel <jason.bittel@gmail.com>
2013-08-28 13:21:02 -0700

    Add alias to admin display and search

--- deflect/admin.py
+++ deflect/admin.py
@@ -49,11 +49,11 @@ class ShortURLAdminForm(forms.ModelForm):
 
 class ShortURLAdmin(admin.ModelAdmin):
     form = ShortURLAdminForm
-    list_display = ('long_url', 'short_url', 'hits', 'last_used', 'creator', 'campaign', 'medium',)
+    list_display = ('long_url', 'short_url', 'shorturlalias', 'hits', 'last_used', 'creator', 'campaign', 'medium',)
     list_filter = ('creator__username', 'campaign', 'medium',)
     ordering = ('-last_used',)
     readonly_fields = ('created', 'short_url', 'qr_code', 'hits', 'last_used',)
-    search_fields = ['long_url', 'campaign',]
+    search_fields = ['long_url', 'campaign', 'shorturlalias__alias',]
     inlines = [ShortURLAliasInline,]
 
     fieldsets = ((None, {'fields': ('long_url', 'short_url',)}),
