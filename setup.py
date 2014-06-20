6bb8c081b512c57cef12988fde38567f1b1a35a6
Jason Bittel <jason.bittel@gmail.com>
2014-06-20 08:42:46 -0700

    Remove versions from setup requirements

--- setup.py
+++ setup.py
@@ -47,8 +47,8 @@ setup(
     ],
     keywords=['django', 'url', 'short url', 'redirect', 'redirection'],
     install_requires=[
-        'base32-crockford==0.2.0',
-        'qrcode==4.0.4',
-        'requests==2.2.1',
+        'base32-crockford',
+        'qrcode',
+        'requests',
     ],
 )
