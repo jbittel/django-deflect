373c9b4452459ed37f06afb15f4730f58ab4f227
Jason Bittel <jason.bittel@gmail.com>
2017-01-18 13:46:50 -0800

    Remove patterns from URLconf

--- deflect/urls.py
+++ deflect/urls.py
@@ -1,9 +1,8 @@
-from django.conf.urls import patterns
 from django.conf.urls import url
 
 from .views import redirect
 
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^(?P<key>[a-zA-Z0-9-]+)$', redirect, name='deflect-redirect'),
-)
+]
